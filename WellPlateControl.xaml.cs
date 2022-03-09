ed3346cdc24564d0fc59f7eba8587bc21ff41d58
JarmoMarczinkowsky <d278529@edu.rocwb.nl>
2022-03-09 10:42:19 +0100

    Fixed #1

---
+++ WellPlateControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WellPlateUserControl
+{
+    /// <summary>
+    /// Interaction logic for WellPlateControl.xaml
+    /// </summary>
+    public partial class WellPlateControl : UserControl
+    {
+        public WellPlateControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
