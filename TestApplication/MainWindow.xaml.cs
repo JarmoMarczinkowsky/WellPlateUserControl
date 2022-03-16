14cc6c0c9e52cc05759263b7646ca4ea0c83be39
JarmoMarczinkowsky <d278529@edu.rocwb.nl>
2022-03-16 12:32:43 +0100

    added comment

--- TestApplication/MainWindow.xaml.cs
+++ TestApplication/MainWindow.xaml.cs
@@ -34,7 +34,7 @@ namespace TestApplication
         private void ConnTest(object sender, RoutedEventArgs e)
         {
             //make sure the colors get set before the grid size
-            globalWellPlate.SetGridColor("Black");
+            globalWellPlate.SetGridColor("Black"); //supports hex, rgb or name simply just a color https://docs.microsoft.com/en-us/dotnet/api/system.windows.media.colors?view=windowsdesktop-6.0
             globalWellPlate.SetClickColor("Red");
             globalWellPlate.SetWellPlateSize(8, 6);
             globalWellPlate.ColorCoordinate("A1;A2;B1;600"); //multiple coordinates get split with a ';'
