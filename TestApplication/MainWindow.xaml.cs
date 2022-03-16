288daa0b194fed3f1c5b72a584e0927e8fdd6b02
JarmoMarczinkowsky <d278529@edu.rocwb.nl>
2022-03-16 10:47:49 +0100

    Added the testapplication

---
+++ TestApplication/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private WellPlateUserControl.Conn _wellPlate = new WellPlateUserControl.Conn();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+
+        }
+
+        private void ConnTest(object sender, RoutedEventArgs e)
+        {
+            _wellPlate.SetWellPlateSize(6, 8);
+            _wellPlate.SetGridColor("Black");
+            _wellPlate.SetClickColor("Red");
+
+            Label myLabel = new Label();
+            myLabel.Content = "Net nieuw";
+            gRefreshTest.Children.Add(myLabel);
+        
+        }
+    }
+}
